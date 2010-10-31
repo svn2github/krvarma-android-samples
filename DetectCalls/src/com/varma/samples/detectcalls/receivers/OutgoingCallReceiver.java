1ba20b0b908c5ef563d6e3e5e20f2ee6cd49a3b0
krvarma <krvarma@fbf79c99-6064-2d75-6e40-39f4ce8e331d>
2010-10-31 08:43:45 +0000

    Initial checkin
    git-svn-id: http://krvarma-android-samples.googlecode.com/svn/trunk@19 fbf79c99-6064-2d75-6e40-39f4ce8e331d

---
+++ DetectCalls/src/com/varma/samples/detectcalls/receivers/OutgoingCallReceiver.java
@@ -0,0 +1,28 @@
+package com.varma.samples.detectcalls.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class OutgoingCallReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Bundle bundle = intent.getExtras();
+		
+		if(null == bundle)
+			return;
+		
+		String phonenumber = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
+
+		Log.i("OutgoingCallReceiver",phonenumber);
+		Log.i("OutgoingCallReceiver",bundle.toString());
+		
+		String info = "Detect Calls sample application\nOutgoing number: " + phonenumber;
+		
+		Toast.makeText(context, info, Toast.LENGTH_LONG).show();
+	}
+}
---
+++ DetectCalls/src/com/varma/samples/detectcalls/ui/MainActivity.java
@@ -0,0 +1,14 @@
+package com.varma.samples.detectcalls.ui;
+
+import com.varma.samples.detectcalls.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
