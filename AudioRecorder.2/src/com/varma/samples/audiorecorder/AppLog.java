a057312bf1492997e8989ee3107c38970c7935f0
krvarma <krvarma@fbf79c99-6064-2d75-6e40-39f4ce8e331d>
2010-12-05 19:22:20 +0000

    Initial checkin
    git-svn-id: http://krvarma-android-samples.googlecode.com/svn/trunk@77 fbf79c99-6064-2d75-6e40-39f4ce8e331d

---
+++ AudioRecorder.2/src/com/varma/samples/audiorecorder/AppLog.java
@@ -0,0 +1,11 @@
+package com.varma.samples.audiorecorder;
+
+import android.util.Log;
+
+public class AppLog {
+	private static final String APP_TAG = "AudioRecorder";
+	
+	public static int logString(String message){
+		return Log.i(APP_TAG,message);
+	}
+}
